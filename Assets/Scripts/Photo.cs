c233e86d59d3755b9454615b3fb65317e8063ea1
xhz636 <240032960@qq.com>
2019-01-26 14:04:28 +0800

    add photo

---
+++ Assets/Scripts/Photo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Photo : Item
+{
+
+    public Photo()
+    {
+        type_ = ItemType.TARGET;
+    }
+
+    public override bool Consume(out bool need_delete)
+    {
+        need_delete = false;
+        return true;
+    }
+}
