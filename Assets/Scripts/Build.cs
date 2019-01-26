864f58a00918665348fec40bdc6b25e26386e258
ysht <ysht1993@163.com>
2019-01-27 00:58:46 +0800

    shaders

---
+++ Assets/Scripts/Build.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Build : MonoBehaviour {
+
+    public Color color;
+    public HighlightingSystem.Highlighter highlighter;
+
+    private void Start()
+    {
+        highlighter = gameObject.AddComponent<HighlightingSystem.Highlighter>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        highlighter.On(color);
+	}
+}
